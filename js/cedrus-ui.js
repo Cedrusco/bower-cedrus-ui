83d846d90587f496c36e07f1b20f448500aa61a6
Mohamed Maher <moh.maher@gmail.com>
2016-12-14 14:48:24 -0500

    release: version 0.2.27

--- js/cedrus-ui.js
+++ js/cedrus-ui.js
@@ -2,12 +2,12 @@
  * Cedrus UI
  * https://github.com/cedrusco/cedrus-ui
  * @license Copyright Cedrus 2016
- * v0.2.26
+ * v0.2.27
  */
 (function( window, angular, undefined ){
 "use strict";
 
-angular.module('cedrus.ui', ["ng","ngAnimate","ngAria","cedrus.ui.core","cedrus.ui.components.calendar","cedrus.ui.components.cdChart","cedrus.ui.components.constant","cedrus.ui.components.cdDateRangePicker","cedrus.ui.components.cdGroupedBarChart","cedrus.ui.components.sidebarFilter","cedrus.ui.components.export"]);
+angular.module('cedrus.ui', ["ng","ngAnimate","ngAria","cedrus.ui.core","cedrus.ui.components.calendar","cedrus.ui.components.cdChart","cedrus.ui.components.constant","cedrus.ui.components.dateRangePicker","cedrus.ui.components.cdGroupedBarChart","cedrus.ui.components.sidebarFilter","cedrus.ui.components.export"]);
 /**
  * @ngdoc module
  * @name cedrus.ui.core
@@ -643,6 +643,15 @@ var cedrus;
     })(ui = cedrus.ui || (cedrus.ui = {}));
 })(cedrus || (cedrus = {}));
 
+/**
+ * @ngdoc module
+ * @name cedrus.ui.components.dateRangePicker
+ */
+/**
+ * @ngdoc directive
+ * @name cdDateRangePicker
+ * @module cedrus.ui.components.dateRangePicker
+ */
 var cedrus;
 (function (cedrus) {
     var ui;
@@ -666,7 +675,7 @@ var cedrus;
                     return DateRangePickerComponent;
                 }());
                 angular
-                    .module('cedrus.ui.components.cdDateRangePicker', [])
+                    .module('cedrus.ui.components.dateRangePicker', [])
                     .component('cdDateRangePicker', new DateRangePickerComponent());
             })(dateRangePicker = components.dateRangePicker || (components.dateRangePicker = {}));
         })(components = ui.components || (ui.components = {}));
@@ -1280,19 +1289,9 @@ var cedrus;
             (function (dateRangePicker) {
                 var DateRangePickerController = (function () {
                     function DateRangePickerController($q, $element) {
-                        var _this = this;
                         this.$q = $q;
                         this.$element = $element;
                         this.today = new Date();
-                        this.animate = function (marginBottom) {
-                            return _this.$q(function (resolve) {
-                                _this.$dateRangeElement.fadeOut(300, function () {
-                                    resolve();
-                                    _this.$dateRangeElement.fadeIn(300);
-                                });
-                                _this.$dateRangeRow.animate({ 'margin-bottom': marginBottom + 'px' }, 600);
-                            });
-                        };
                         this.defaultOptions = {
                             startDate: {
                                 min: false,
@@ -1303,6 +1302,7 @@ var cedrus;
                                 max: false,
                                 placeholder: 'Date To'
                             },
+                            useDateRange: false,
                             errorMessages: {
                                 startRequired: 'Date From is required when using a date range.',
                                 endRequired: 'Date To is required when using a date range.'
@@ -1310,8 +1310,6 @@ var cedrus;
                         };
                     }
                     DateRangePickerController.prototype.$onInit = function () {
-                        this.$dateRangeElement = this.$element;
-                        this.$dateRangeRow = this.$element.find('#date-range-row');
                         this.$startInputContainer = this.$element.find('.startDate');
                         this.options = angular.extend({}, this.defaultOptions, this.options);
                         // user may provide an initalization call back which can be used to subscribe to any necessary events or otherwise
@@ -1324,32 +1322,36 @@ var cedrus;
                         }
                         ;
                         this.parentFormSubmitted = false;
-                        this.useDateRange = false;
+                        this.useDateRange = !!this.options.useDateRange;
                     };
                     // click function that toggles daterange on/off and runa
                     DateRangePickerController.prototype.toggleDateRange = function (form) {
-                        var _this = this;
-                        var marginBottom = this.useDateRange ? 0 : 55;
-                        return this.animate(marginBottom)
-                            .then(function () {
-                            if (_this.useDateRange) {
-                                _this.useDateRange = false;
-                                _this.endDate = undefined;
-                                form.startDate.$setPristine();
-                                form.$$parentForm.$submitted = false;
-                                _this.onDatepickerChange(_this.startDate);
-                                _this.$startInputContainer.removeClass('md-datepicker-invalid');
-                            }
-                            else {
-                                // console.log(this.form.endDate);
-                                _this.useDateRange = true;
-                            }
-                        });
+                        if (this.useDateRange) {
+                            this.useDateRange = false;
+                            this.endDate = undefined;
+                            form.startDate.$setPristine();
+                            form.$$parentForm.$submitted = false;
+                            this.onDatepickerChange(this.startDate);
+                            this.$startInputContainer.removeClass('md-datepicker-invalid');
+                        }
+                        else {
+                            // console.log(this.form.endDate);
+                            this.useDateRange = true;
+                        }
                     };
                     DateRangePickerController.prototype.onDatepickerChange = function (startDate, endDate) {
                         this.ngModel.$setViewValue({ startDate: startDate, endDate: endDate || startDate });
                         this.ngModel.$render();
                     };
+                    // animate = (marginBottom) => {
+                    // return this.$q((resolve) => {
+                    //     this.$dateRangeElement.fadeOut(300, () => {
+                    //         resolve();
+                    //         this.$dateRangeElement.fadeIn(300);
+                    //     });
+                    //     this.$dateRangeRow.animate({ 'margin-bottom': marginBottom + 'px' }, 600);
+                    // });
+                    // }
                     DateRangePickerController.prototype.getToggleDateRangeText = function (useDateRange) {
                         return useDateRange ?
                             'Use Single Date' :
@@ -1373,7 +1375,7 @@ var cedrus;
                     return DateRangePickerController;
                 }());
                 angular
-                    .module('cedrus.ui.components.cdDateRangePicker')
+                    .module('cedrus.ui.components.dateRangePicker')
                     .controller('CdDateRangePicker', DateRangePickerController);
             })(dateRangePicker = components.dateRangePicker || (components.dateRangePicker = {}));
         })(components = ui.components || (ui.components = {}));
@@ -1723,7 +1725,7 @@ var CdWorksheetExport;
 })(CdWorksheetExport || (CdWorksheetExport = {}));
 
 angular.module('cedrus.ui').run(['$templateCache', function($templateCache) {$templateCache.put('components/calendar/calendar.tpl.html','<div class="container"><div class="hider" ng-click="vm.displayCal()" ng-show="vm.showCal"></div><div class="input-blocker" ng-click="vm.displayCal()" readonly="true"></div><input type="text" class="md-select" ng-model="vm.selection"><div ng-show="vm.showCal" class="panel"><div class="md-whiteframe-2dp"><div class="yearrow" layout="row" layout-align="space-between end"><md-button class="calbtn" ng-click="vm.setYear(-1)" ng-hide="vm.yearSel" aria-label="previous year"><i class="fa fa-chevron-left"></i></md-button><md-button class="calbtn" ng-click="vm.setYear(-12)" ng-show="vm.yearSel" aria-label="go back one page"><i class="fa fa-chevron-left"></i></md-button><md-button class="calbtn yearbtn" ng-click="vm.flipCal()" aria-label="swap between month and year select">{{vm.date.selYear || vm.initYear}}</md-button><md-button class="calbtn" ng-click="vm.setYear(1)" ng-hide="vm.yearSel" aria-label="next year"><i class="fa fa-chevron-right"></i></md-button><md-button class="calbtn" ng-click="vm.setYear(12)" ng-show="vm.yearSel" aria-label="go forward by one page"><i class="fa fa-chevron-right"></i></md-button></div><div layout="row" ng-repeat="monthRow in vm.monthMap" ng-hide="vm.yearSel"><md-button class="calbtn monthSelect" ng-repeat="month in monthRow" ng-click="vm.setMonth(month.value)" aria-label="choose {{month.value}}">{{month.display}}</md-button></div><div layout="row" ng-repeat="yearRow in vm.yearMap" ng-show="vm.yearSel"><md-button class="calbtn yearSelect" ng-repeat="year in yearRow" ng-click="vm.flipCal(); vm.setYear(year)" aria-label="choose {{year}}">{{(vm.date.selYear||vm.initYear)+year}}</md-button></div></div></div></div>');
-$templateCache.put('components/date-range-picker/date-range-picker.tpl.html','<div ng-form="vm.form"><md-datepicker name="startDate" ng-model="vm.startDate" ng-change="vm.onDatepickerChange(vm.startDate, vm.endDate)" ng-required="vm.shouldRequire(vm.useDateRange, vm.endDate)" md-placeholder="{{ vm.startPlaceholder(vm.useDateRange) }}" md-min-date="vm.options.startDate.min || false" md-max-date="vm.endDate || vm.options.startDate.max || false" class="startDate"></md-datepicker><md-datepicker ng-show="vm.useDateRange" name="endDate" ng-model="vm.endDate" ng-change="vm.onDatepickerChange(vm.startDate, vm.endDate)" ng-required="vm.shouldRequire(vm.useDateRange, vm.startDate)" md-placeholder="{{ vm.options.endDate.placeholder }}" md-min-date="vm.startDate" md-max-date="vm.options.endDate.max" class="last-datepicker"></md-datepicker></div><a ng-click="vm.toggleDateRange(vm.form)">{{ vm.getToggleDateRangeText(vm.useDateRange) }}</a><div class="error-div" ng-if="vm.useDateRange && vm.form.$error && vm.parentFormSubmitted"><span ng-if="vm.form.startDate.$error.required">{{ vm.options.errorMessages.startRequired }} </span><span ng-if="vm.form.endDate.$error.required">{{ vm.options.errorMessages.endRequired }}</span></div>');
+$templateCache.put('components/date-range-picker/date-range-picker.tpl.html','<div class="date-range-picker"><div ng-form="vm.form" class="ng-cloak"><md-datepicker name="startDate" class="startDate" ng-model="vm.startDate" ng-change="vm.onDatepickerChange(vm.startDate, vm.endDate)" ng-required="vm.shouldRequire(vm.useDateRange, vm.endDate)" md-placeholder="{{ vm.startPlaceholder(vm.useDateRange) }}" md-min-date="vm.options.startDate.min || false" md-max-date="vm.endDate || vm.options.startDate.max || false" class="startDate"></md-datepicker><md-datepicker ng-show="vm.useDateRange" name="endDate" ng-class="{hidden: !vm.useDateRange}" ng-model="vm.endDate" ng-change="vm.onDatepickerChange(vm.startDate, vm.endDate)" ng-required="vm.shouldRequire(vm.useDateRange, vm.startDate)" md-placeholder="{{ vm.options.endDate.placeholder }}" md-min-date="vm.startDate" md-max-date="vm.options.endDate.max" class="endDate" ng-cloak></md-datepicker></div><a class="toggle-link" ng-click="vm.toggleDateRange(vm.form)">{{ vm.getToggleDateRangeText(vm.useDateRange) }}</a><div class="error-div" ng-if="vm.useDateRange && vm.form.$error && vm.parentFormSubmitted"><span ng-if="vm.form.startDate.$error.required">{{ vm.options.errorMessages.startRequired }} </span><span ng-if="vm.form.endDate.$error.required">{{ vm.options.errorMessages.endRequired }}</span></div></div>');
 $templateCache.put('components/grouped-bar-chart/grouped-bar-chart.tpl.html','<div class="cd-grouped-bar-chart"><div ng-hide="vm.showData()"><div layout="row" layout-fill layout-align="center center" class="no-data"><span>There are no active tasks.</span></div></div><div ng-show="vm.showData()"><div ng-repeat="group in vm.groupDataKeys"><div layout="row" class="group-item"><div layout="column"><md-icon ng-hide="vm.expandField(group)" md-font-icon="fa fa-caret-right" ng-click="vm.setExpandedField(group)" ng-if="vm.options.subFields"></md-icon><md-icon ng-show="vm.expandField(group)" md-font-icon="fa fa-caret-down" ng-click="vm.setExpandedField(group, true)" ng-if="vm.options.subFields"></md-icon></div><div layout="column" flex><div layout="row" layout-align="space-around none"><div flex="60" layout-align="start center">{{group}}</div><div flex="20">Count: {{vm.groupData[group].length}}</div><div flex="20">{{vm.groupData[group].length*100/vm.totalKeys | number:0}}%</div></div></div></div><div layout="row" flex class="line-color"><div ng-style="{width:vm.groupData[group].length*100/vm.totalKeys + \'%\', \'background\': vm.getColor($index, group)}" class="red-line"></div></div><div class="data-container" ng-show="vm.expandField(group)"><div layout="column" ng-show="vm.expandField(group)" ng-if="vm.options.subFields"><div ng-repeat="el in vm.groupData[group]"><div ng-include="vm.options.extendedTemplate || \'lineChartSingleItemExpanded\'"></div></div></div></div></div><div layout="row" layout-align="end none" class="total">Total Count:{{vm.totalKeys}}</div></div></div><script type="text/ng-template" id="lineChartSingleItemExpanded"><div class="panel" layout="column" layout-align="center none">\n        <div layout="row" layout-align="space-between none" class="data-row">\n            <div ng-repeat="(field, displayText ) in vm.options.subFields">\n                <span class="bold-text">{{displayText}}</span>\n                <span>{{el[field]}}</span>\n            </div>\n        </div>\n    </div></script>');
 $templateCache.put('components/sidebar-filter/sidebar-filter.tpl.html','<!--implemenation for user provided custom type/templates--><!--change naming to filter-tree, side-filter towards end--><div class="cd-sidebar-filter"><div ng-repeat="group in vm.filterGroups track by $index" ng-class="(vm.groupLevelClasses + (group.customClass ? \' \' + group.customClass : \'\') )"><div ng-if="!vm.options.isFlat"><md-button ng-click="vm.toggleExpand(group)" class="md-icon-button" aria-label="expand"><md-icon md-font-set="fa" md-font-icon="fa-chevron-right" ng-class="(group.isExpanded  !== false )? \'fa-chevron-down\': \'fa-chevron-right\'"></md-icon></md-button><span>{{ ::vm.processTitle(group, 0) }}</span></div><ul ng-show="group.isExpanded !== false" layout="column" ng-repeat="(filterName, filter) in vm.filters[group.key] track by filterName" ng-class="(vm.filterLevelClasses + (filter.customClass ? \' \' + filter.customClass : \'\'))" ng-class="{ cdFilterFlat : vm.options.isFlat}"><li><div><md-button ng-click="vm.toggleExpand(filter)" class="md-icon-button" aria-label="expand"><md-icon md-font-set="fa" md-font-icon="fa-chevron-right" ng-class="(filter.isExpanded  !== false )? \'fa-chevron-down\': \'fa-chevron-right\'"></md-icon></md-button><span>{{::vm.processTitle(filter, 1)}}</span><ul ng-show="filter.isExpanded !== false"><div ng-if="filter.type === \'checkbox\'" ng-include="\'cdCheckBoxFilter\'"></div><div ng-if="filter.type !== \'checkbox\'" ng-include="vm.customFields[filter.type].template"></div></ul></div></li></ul></div></div><script type="text/ng-template" id="cdCheckBoxFilter"><li ng-repeat="(optionName, option) in filter.options track by optionName" ng-class="(vm.optionLevelClasses + (option.customClass ? \' \' + option.customClass : \'\'))">\n        <md-checkbox ng-model="option.isSelected" ng-change="vm.changeFilter(filter, optionName, $index)" class="md-primary" ng-model-options="{debounce: 250}"\n            aria-label="{{::vm.processTitle(option, 2)}}">\n            <span>{{::vm.processTitle(option, 2)}}</span>\n        </md-checkbox>\n    </li></script>');}]);
-})(window, window.angular);;window.cedrusUI={version:{full: "0.2.26"}};
+})(window, window.angular);;window.cedrusUI={version:{full: "0.2.27"}};
